e070aae5ff01b866c8f21d1d98f090992dc77b91
Linas <linas.satkauskas000@gmail.com>
2025-09-30 01:51:03 +0300

    4to lab. pradžia

---
+++ ReactApp1.Server/Services/GetStudentService.cs
@@ -0,0 +1,11 @@
+﻿using ReactApp1.Server.Models.DTOs;
+
+namespace ReactApp1.Server.Services;
+
+public interface IGetStudentService
+{
+    Task<List<StudentDTO>> GetAll();
+    Task<StudentDTO> Get(int id);
+}
+
+
